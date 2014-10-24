f081876cc188247a8e5e87037bfeca60d650240a
Adam Enger <aenger@opinionlab.com>
2014-10-23 20:46:02 -0500

    cleaning up snipper class and adding comments

--- lib/snipper.rb
+++ lib/snipper.rb
@@ -1,28 +1,43 @@
+# Class to be instantiated which handles the bulk of list processing
 class Snipper  
   def initialize(options)  
-    # Instance variables  
+    # this is the hash of options which comes from the opts parser
     @options = options
+
+    # the filename which we've passed using -f 
     @filename = @options[:file]
-    @file_array = Array.new
+    
+    # empty array which is intended to be filled with musicians
     @people_array = Array.new
+
+    # This is the file we'll open with the -f flag
     tmp_file = File.open(@filename).each_line do |f|
       @file_array << f.chomp 
       self.guess_delimiter(f)
     end
+    # close the file after we're done with it
     tmp_file.close
-  end  
 
+  end  
+  
+  # this function will determine which delimiter is used on a specific line in a file
   def guess_delimiter(string)
+
+    # match just on a space delimited line
     if /\w \w/.match(string)
       if @options[:verbose]
         puts "DEBUG: Matched on space"
-      end   
+      end 
       self.split_line(string, " ")
+
+    # match on comma delimited line
     elsif /,\s*/.match(string)
       if @options[:verbose]
         puts "DEBUG: Matched on comma"
       end   
       self.split_line(string, ",")
+
+    # match on pipe delimited line
     elsif /\w \| \w/.match(string)
       if @options[:verbose]
         puts "DEBUG: Matched on pipe"
@@ -30,8 +45,10 @@ class Snipper
       self.split_line(string, "|")
     end
   end
-
+  
+  # after we've determined which delimiter the line is using, this function will split and add it to people_array
   def split_line(line, delimiter)
+
     # split the given line and add it to the people array
     split_line = line.split(delimiter)
 
@@ -48,10 +65,7 @@ class Snipper
     @people_array << name_hash
   end
 
-  def people
-    @people_array
-  end
-
+  # method which is intended to be used for pretty printing list of musicians
   def print
       table_header = "Last Name | First Name | Gender | Date of Birth | Favorite Color"
       puts table_header
