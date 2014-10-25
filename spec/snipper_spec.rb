ad446f3f5511cf99cb5be37e0ff1c86f74905052
Adam Enger <aenger@opinionlab.com>
2014-10-25 02:35:08 -0500

    adding tests for sort, process_line

--- spec/snipper_spec.rb
+++ spec/snipper_spec.rb
@@ -6,6 +6,8 @@ describe 'The Snipper' do
     options = Hash.new
     options[:file] = "examples/spec-test.txt"
     s = Snipper.new(options[:file]) 
+    s.read_file
+    processed_lines = s.process_lines(s.instance_variable_get(:@file_array))
 
     it "uses a string to initialize" do
       expect(s.instance_variable_get(:@filename)).to eq("examples/spec-test.txt")
@@ -45,8 +47,25 @@ describe 'The Snipper' do
     end
   
     it "can load a file into memory which matches file on disk" do
-      s.read_file
       # assumes unix environment
       expect(%x{wc -l #{s.instance_variable_get(:@filename)}}.split.first.to_i).to eq(s.instance_variable_get(:@file_array).length)
+    end 
+  
+    it "can process an array of lines from the loaded file and return an array" do
+      expect(processed_lines.class).to eq(Array)
+    end
+  
+    it "array contains a hash after processing lines" do
+      expect(s.process_lines(s.instance_variable_get(:@file_array))[0].class).to eq(Hash)
+    end
+
+    it "can sort the loaded file and return an array" do
+      sorted = s.sort(processed_lines, "LastName")
+      expect(sorted.class).to eq(Array)
+    end 
+    
+    it "sorted array contains hash" do
+      sorted = s.sort(processed_lines, "LastName")
+      expect(sorted[0].class).to eq(Hash)
     end 
 end
